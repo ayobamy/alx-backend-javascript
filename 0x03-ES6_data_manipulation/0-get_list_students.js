98691f76a61f839d6cd1e93828fbb43e8ca0c6da
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-01-02 17:32:08 +0000

    [update]: fixed bugs

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
-  const studentList = [
+  let studentList = [
     {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
+      'id': 1,
+      'firstName': 'Guillaume',
+      'location': 'San Francisco',
     },
     {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
+      'id': 2,
+      'firstName': 'James',
+      'location': 'Columbia',
     },
     {
-      id: 3,
-      firstName: 'Serena',
-      location: 'San Francisco',
+      'id': 3,
+      'firstName': 'Serena',
+      'location': 'San Francisco',
     },
   ];
 
