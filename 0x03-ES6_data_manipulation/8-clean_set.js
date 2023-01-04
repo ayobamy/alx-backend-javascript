d4b6a3c21a02656c59408a38786a18526171e729
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-01-04 01:00:45 +0000

    [update]: clean set of string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,15 @@
 export default function cleanSet(set, startString) {
-  
+  let str = '';
+  const result = [];
+  const strLen = startString.length;
+
+  if ((startString) && (typeof startString === 'string')) {
+    for (const elem of set) {
+      if (elem && elem.startsWith(startString)) {
+        result.push(elem.slice(strLen));
+      }
+    }
+    str = result.join('-');
+  }
+  return str;
 }
