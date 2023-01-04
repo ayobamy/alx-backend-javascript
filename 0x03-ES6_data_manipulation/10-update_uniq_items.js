001508d1e097c87b657967987b3adf274c908e04
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-01-04 01:50:53 +0000

    [update]: fixed bug

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(mapUpdate) {
-  for (const [key, value] of mapUpdate) {
-    if (value === 1) {
-      mapUpdate.set(key, 100);
-    } else mapUpdate.set(key, value);
-  }
-
   if (!(mapUpdate instanceof Map)) {
     throw new Error('Cannot process');
+  } else {
+    for (const [key, value] of mapUpdate) {
+      if (value === 1) {
+        mapUpdate.set(key, 100);
+      }
+    }
   }
 
   return mapUpdate;
