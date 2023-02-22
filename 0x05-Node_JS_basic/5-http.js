0fb62c7a4790ba79b575b7dbd4b21cd2a251f759
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-22 13:35:05 +0000

    [fix]: fixed bug

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-// const fs = require('fs');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
