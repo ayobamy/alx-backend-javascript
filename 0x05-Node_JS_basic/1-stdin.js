444022c8afeb220d607ca5804b44e76b7609855f
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-22 03:03:18 +0000

    [fix]: removed comments

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,12 @@
-// displays message to the user
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// set encoding to utf-8 encoding
 process.stdin.setEncoding('utf8');
-// reading user input from the console
 process.stdin.on('readable', () => {
   const userInput = process.stdin.read();
-  // check user input
   if (userInput) {
     process.stdout.write(`Your name is: ${userInput}`);
   }
 });
-// exit message
+
 process.stdin.on('end', () => {
   process.stdin.write('This important software is now closing\n');
 });
