444477cf3b57c3b76d47aac9fe090fcf6e61a8cd
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-24 11:07:03 +0000

    [fix]: fixed import bug

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,29 +1,29 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const calculateNumber = require("./2-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", function () {
   describe("ADDITION", function () {
-    it("should return 6 when 1.4 and 4.5 are added", function () {
+    it("should return 6 = 1.4 + 4.5", function () {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
     });
   });
   
   describe('SUBTRACTION', function () {
-    it("should return -4 when 4.5 is subtracted from 1.4", function () {
+    it("should return -4 = 1.4 - 4.5", function () {
       expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     })
   });
 
   describe('DIVISION', function () {
-    it("should return 0.2 when 1.4 is divided by 4.5", function () {
+    it("should return 0.2 = 1.4 / 4.5", function () {
       expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     })
   });
 
   describe('DIVISION', function () {
-    it("should return Error when 1.4 is divided by 0", function () {
+    it("should return Error =  1.4 / 0", function () {
       expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     })
   });
