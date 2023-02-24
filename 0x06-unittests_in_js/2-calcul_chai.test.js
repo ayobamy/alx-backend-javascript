4d997647949a2782ab5b3f8e0bbbc6b012afe316
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-24 02:44:42 +0000

    [feat]: test for updated calculateNumber function using chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,28 +1,30 @@
-const assert = require("assert");
+const chai = require("chai");
+const expect = chai.expect;
+
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function () {
   describe("ADDITION", function () {
     it("should return 6 when 1.4 and 4.5 are added", function () {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
     });
   });
   
   describe('SUBTRACTION', function () {
     it("should return -4 when 4.5 is subtracted from 1.4", function () {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     })
   });
 
   describe('DIVISION', function () {
     it("should return 0.2 when 1.4 is divided by 4.5", function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     })
   });
 
   describe('DIVISION', function () {
     it("should return Error when 1.4 is divided by 0", function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     })
   });
 });
