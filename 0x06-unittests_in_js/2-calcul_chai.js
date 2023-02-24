c058fd1c915177ad8d1d20322cc1fbccd2a01b09
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-24 02:44:22 +0000

    [update]: updated arithmetic function

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -15,19 +15,6 @@ function calculateNumber(type, a, b) {
     default:
       return 'Error';
   }
-  // if (type == 'SUM') {
-  //   return roundA + roundB;
-  // } 
-  // if (type == 'SUBTRACT') {
-  //   return roundA - roundB;
-  // } 
-  // if (type == 'DIVIDE') {
-  //   if (roundB == 0) {
-  //     return('Error');
-  //   } else {
-  //     return roundA / roundB;
-  //   }
-  // }
 }
 
 module.exports = calculateNumber;
