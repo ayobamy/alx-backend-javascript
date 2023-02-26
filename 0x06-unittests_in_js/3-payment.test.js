9cb9b36faed159b5ef3672792851a52fea9882ab
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-26 20:38:31 +0000

    [fix]: fixed

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,4 +1,5 @@
 const sinon = require('sinon');
+const chai =  require('chai');
 const sendPaymentRequestToApi = require("/3-payment");
 const Utils = require('./utils');
 
@@ -6,8 +7,8 @@ describe('sendPaymentRequestToApi', () => {
   it('calls Utils.calculateNumber with SUM type and 100, 20 as arguments', () => {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledOnce(calculateNumberSpy);
-    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-    calculateNumberSpy.restore();
+    expect(calculateNumberSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.calculateNumber.calledOnce).to.be.true;
+    calculateNumberSpy.calculateNumber.restore();
   });
 });
